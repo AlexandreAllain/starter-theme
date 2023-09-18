a63ddc7a46c58398675bdf58738f97caa7590e95
Alexandre Allain <alexandre.allain@lunaweb.fr>
2023-09-18 10:29:34 +0200

    Add an env config

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,31 @@
+require('dotenv').config();
+
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
 const concat = require('gulp-concat');
-const browserSync = require('browser-sync').create(); // Importez BrowserSync
+const browserSync = require('browser-sync').create();
+
+// Accédez aux variables d'environnement définies dans le fichier .env
+const localServer = process.env.LOCAL_SERVER || 'http://localhost:8888/starter-theme/';
 
 function compileSass() {
-  return gulp
-    .src('ALSite/source/scss/test.scss') // Chemin vers le fichier de test SASS
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer())
-    .pipe(concat('style.css')) // Nommez le fichier de sortie
-    .pipe(gulp.dest('./')) // Répertoire de sortie à la racine
-    .pipe(browserSync.stream()); // Rafraîchir le navigateur
+    return gulp
+        .src('ALSite/source/scss/test.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest('./'))
+        .pipe(browserSync.stream());
 }
 
 function watchSass() {
     browserSync.init({
-        proxy: 'http://localhost:8888/starter-theme/', // Adresse de votre serveur local
+        proxy: localServer,
     });
 
-  gulp.watch('ALSite/source/scss/**/*.scss', compileSass); // Surveillez tous les fichiers SASS
-  gulp.watch('*.html').on('change', browserSync.reload); // Surveillez les fichiers HTML pour le rafraîchissement
+    gulp.watch('ALSite/source/scss/**/*.scss', compileSass);
+    gulp.watch('*.html').on('change', browserSync.reload);
 }
 
 exports.compileSass = compileSass;
