ab9ccba30a71ae134112a0e14b7d5b0b89728513
Alexandre <alexandre.allain1@gmail.com>
2024-02-25 19:54:38 +0100

    feat(skiplinks): début du dev

--- functions.php
+++ functions.php
@@ -172,6 +172,13 @@ new StarterSite();
 
 error_reporting(E_ALL ^ E_DEPRECATED);
 
+// Supprime le style des blocks
+
+function remove_block_library_css() {
+    wp_dequeue_style('wp-block-library');
+}
+add_action('wp_enqueue_scripts', 'remove_block_library_css');
+
 // Ajoute du style au debug 
 
 require __DIR__ . '/vendor/autoload.php';
